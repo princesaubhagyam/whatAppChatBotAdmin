8ac587e35d8cb1ca5a885e0a7a39a9f0bb6b079e
princesaubhagyam <prince@saubhagyam.org>
2024-08-29 16:06:22 +0530

    success payment

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,12 +1,14 @@
 import { useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
 import { Button, Card, CircularProgress, Typography } from '@mui/material';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import apiClient from 'src/api/axiosClient';
 
 const CheckoutForm = ({ isHandleClose, clientSecret, amount }) => {
   const stripe = useStripe();
   const elements = useElements();
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -17,50 +19,46 @@ const CheckoutForm = ({ isHandleClose, clientSecret, amount }) => {
 
     setLoading(true);
 
-    // First, submit the payment form
-    const { error: submitError } = await elements.submit();
+    try {
+      // Confirm the payment with Stripe
+      const result = await stripe.confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: window.location.origin, // Temporary URL or null
+        },
+        redirect: 'if_required',
+      });
 
-    if (submitError) {
-      console.log(submitError.message);
-      setLoading(false);
-      return;
-    }
-
-    // Now, confirm the payment
-    const result = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        // Use this to confirm the payment without redirecting immediately
-        return_url: window.location.origin, // Temporary URL or null
-      },
-      redirect: 'if_required', // Prevent automatic redirection
-      // confirmParams: {
-      //   return_url: 'https://chatbot.saubhagyam.net/payment-success',
-      // },
-    });
-
-    if (result.error) {
-      console.log(result.error.message);
-      setLoading(false);
-    } else if (result.paymentIntent.status === 'succeeded') {
-      console.log('Payment successful');
-      const stripeTransactionId = result.paymentIntent.id;
-
-      try {
-        const response = await apiClient.post('/add_money_wallet/', {
-          amount: amount,
-          stripe_transaction_id: stripeTransactionId,
-        });
-
-        console.log('Wallet updated successfully', response.data);
-      } catch (apiError) {
-        console.log('Failed to update wallet', apiError);
-        // Handle the failed wallet update scenario here
+      if (result.error) {
+        console.log(result.error.message);
+        setLoading(false);
+        return;
       }
 
-      setLoading(false);
-    } else {
-      console.log('Payment not completed');
+      if (result.paymentIntent.status === 'succeeded') {
+        console.log('Payment successful');
+        const stripeTransactionId = result.paymentIntent.id;
+
+        
+        try {
+          const response = await apiClient.post('/add_money_wallet/', {
+            amount: amount,
+            stripe_transaction_id: stripeTransactionId,
+          });
+
+          console.log('Wallet updated successfully', response.data);
+
+          navigate('/payment-success');
+        } catch (apiError) {
+          console.log('Failed to update wallet', apiError);
+          
+        }
+      } else {
+        console.log('Payment not completed');
+      }
+    } catch (error) {
+      console.error('Error during payment processing:', error);
+    } finally {
       setLoading(false);
     }
   };
